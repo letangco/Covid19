0e4134074babb4cc77f39338930302f1bd9676ea
letangco <letangco@gmail.com>
2020-06-14 19:12:32 +0700

    Hiển thị xong các thuộc tính của Map bằng jVector nhưng chưa CSS cần CSS lại

--- src/components/mapDistribute/mapGeoWorld.tsx
+++ src/components/mapDistribute/mapGeoWorld.tsx
@@ -6,6 +6,12 @@ import { VectorMap } from 'react-jvectormap';
 // API "https://api.covid19api.com/all" 
 interface Istate{
     dataGeoMap: any[],
+    dataConfirmed: {},
+    dataDeath: {},
+    dataRecovered: {},
+    dataNewConfirmed: {},
+    dataNewDeath: {},
+    dataNewRecovered: {}
 }
 class MapGeoChart extends Component <{},Istate>{
     
@@ -14,6 +20,12 @@ class MapGeoChart extends Component <{},Istate>{
         super(props);
         this.state={
             dataGeoMap: [],
+            dataConfirmed :{},
+            dataDeath: {},
+            dataRecovered: {},
+            dataNewConfirmed: {},
+            dataNewDeath: {},
+            dataNewRecovered: {}    
         }
         
     }
@@ -33,11 +45,36 @@ class MapGeoChart extends Component <{},Istate>{
             var A = res.data.Countries;
             // console.log(res.data.Countries);
             dataGeoMap.push(temp);
+
+            var TempConfirmed :any={},TempDeath:any={};
+            var TempDeath:any ={};
+            var TempRecovered:any = {};
+            var TemNewConfirmed:any ={};
+            var TemNewDeath:any ={};
+            var TemNewRecovered:any ={};
             for (var i=0; i<A.length; i++)
             {
-                // console.log(Object.values(A[i]));
+                // console.log(Object.values(A[i])[1]);
+                var countryCode:any = Object.values(A[i])[1];
+                countryCode = countryCode.toString();
+                // console.log(countryCode)
+                var tempObjConfirmed :any ={[countryCode] : Object.values(A[i])[4]};
+                var temObjDeath:any = {[countryCode]:Object.values(A[i])[6]};
+                var tempObjRecovered = {[countryCode]:Object.values(A[i])[8]};
+                var tempObjNewConfirmed = {[countryCode]:Object.values(A[i])[3]};
+                var tempObjNewDeath = {[countryCode]:Object.values(A[i])[5]};
+                var tempObjNewRecovered = {[countryCode]:Object.values(A[i])[7]};
+
+                TempConfirmed = Object.assign(TempConfirmed,tempObjConfirmed);
+                TempDeath = Object.assign(TempDeath,temObjDeath);
+                TempRecovered = Object.assign(TempRecovered,tempObjRecovered);
+                TemNewConfirmed = Object.assign(TemNewConfirmed,tempObjNewConfirmed);
+                TemNewDeath = Object.assign(TemNewDeath,tempObjNewDeath);
+                TemNewRecovered = Object.assign(TemNewRecovered,tempObjNewRecovered);
+                // tempObjConfirmed={};
+
+                // Sử dụng cho GeoChart
                 var trans = Object.values(A[i]);
-                
                 tempData.push(trans[0]);
                 tempData.push(trans[4]);
                 tempData.push(trans[3]);
@@ -48,36 +85,78 @@ class MapGeoChart extends Component <{},Istate>{
                 // tempData.push(trans[9]);
                 dataGeoMap.push(tempData);
                 tempData =[];
-                
             }
+            // console.log(TempConfirmed)
             this.setState({
-                dataGeoMap: dataGeoMap
+                dataGeoMap: dataGeoMap,
+                dataConfirmed: TempConfirmed,
+                dataDeath: TempDeath,
+                dataRecovered: TempRecovered,
+                dataNewConfirmed: TemNewConfirmed,
+                dataNewDeath: TemNewDeath,
+                dataNewRecovered: TemNewRecovered
             })
         })
     }
+    
     render ()
     {
+        
+        let chkValue = (val1 :any): any => {
+            var x;
+            if (typeof val1 ==="undefined"){
+                x = 0;
+                // return 0;
+            }
+            else {
+                x=val1;
+                // return val1;
+            }
+            // console.log(x)
+            return x;
+        }
+        function getDataConfirmed(data:any)
+        {
+            data = TempConfirmed;
+            // console.log(data);
+            return data;
+        }
+        function getFlagImgSrc(code:any)
+        {
+            var code = code.toLowerCase();
+        return 'https://cdn.jsdelivr.net/gh/hjnilsson/country-flags@latest/svg/'+code+'.svg';
+        }   
+        function onRegionLabelShow(event :any , label :any, code :any){
+            // var cdata :any = {US: "35", NO: "15", "SE": "17", "GB": "20", "ES": "10"};
+            // var cdata2 :any = {US: "40", NO: "35", "SE": "17", "GB": "20", "ES": "10"};
+            var Confirmed :any= TempConfirmed;
+            var Death:any = TempDeath;
+            var Recovered:any = TempRecovered;
+            var newConfirmed:any = TempNewConfirmed;
+            var newDeath:any = TempNewDeath;
+            var newRecovered:any = TemNewRecovered;
+            // console.log(Confirmed)
+        //     label.html(label.html() +' (' + code.toString() + 
+        //    ') <br>Số Count: ' + Confirmed[code]);
+           label.html('<img src="'+getFlagImgSrc(code)+'" height="80" >'+'<br />'+ label.html() + 
+           '<hr style="height: 1px"/><p style="color: red">Total Confirmed:'+  Confirmed[code]+'</p> ' 
+            + '<p style="color: yellow">Total Death:'+  Death[code]+'</p>' + '<p style="color: #66ff66">Total Recovered:'+  Recovered[code]+'</p>'
+           + '<p style="color:#ff6600">Total New Confirmed:'+  newConfirmed[code]+'</p>' + '<p style="color: #ff5050">Total New Death:'+  newDeath[code]+'</p>' 
+           + '<p style="color:#3366ff">Total New Recovered:'+  newRecovered[code]+'</p>'
+           );
+            
+        }
+        var TempConfirmed :any = this.state.dataConfirmed;
+        var TempDeath:any = this.state.dataDeath;
+        var TempRecovered:any = this.state.dataRecovered;
+        var TempNewConfirmed:any = this.state.dataNewConfirmed;
+        var TempNewDeath:any = this.state.dataNewDeath;
+        var TemNewRecovered :any= this.state.dataNewRecovered;
+        // console.log(TempConfirmed)
+        var cdata :any = {"US": "35", "NO": "15", "SE": "17", "GB": "20", "ES": "10"};
         var dataGeoMap = this.state.dataGeoMap;
-
         return (
             <div className = "container-fluid">
-                {/* <Chart
-                    width={'100%'}
-                    height={'100%'}
-                    chartType="GeoChart"
-                    data={dataGeoMap}
-                    options={{
-                        
-                        colorAxis: { colors: ['green','yellow','orange','pink', 'red'] },
-                      }}
-                    // data = {
-                    //   ['',''],....
-                    //}
-                    // Note: you will need to get a mapsApiKey for your project.
-                    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                    // mapsApiKey="AIzaSyAAji08D-qvQDAreTvqrv7pz_UHOJFz2gA"
-                    // rootProps={{ 'data-testid': '1' }}
-                /> */}
                 
                 <div className = "container-fluid" >
                     
@@ -89,19 +168,43 @@ class MapGeoChart extends Component <{},Istate>{
                                 <VectorMap
                                 map='world_mill'
                                 backgroundColor="#0071A4"
+                                
                                 useRef ={"map"}
                                 containerStyle={{
                                     width: '100%',
                                     height: '100%'
                                 }}
                                 containerClassName="map"
-                                scale ={ ['#C8EEFF', '#0071A4']}
+                                scale ={ ['#99ff33','#669900','#ffcc00','#cc6600']}
                                 hoverOpacity= {0.8}
-                                
+                                regionStyle = {
+                                    {
+                                        hover: {
+                                        "fill-opacity": 0.8,
+                                        cursor: 'pointer'
+                                        },
+                                        selected: {
+                                           stroke: '#000',
+                                           "stroke-width": 1,
+                                           "stroke-opacity": 1
+                                        }
+                                     }
+                                }
+                                series= {
+                                    {
+                                        regions: [{
+                                            values: TempConfirmed,
+                                            scale: ['#ffcc00','#cc6600','#ff3300', '#ff0000'],
+                                            normalizeFunction: 'polynomial'
+                                          }]
+                                    }
+                                  }
+                                  onRegionTipShow = {onRegionLabelShow}
                                 />
                             </div>
+                            
                             <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-                                
+                            
                         </div>
                     </div>
                     
@@ -135,13 +238,19 @@ class MapGeoChart extends Component <{},Istate>{
                                 datalessRegionColor: '#ff3300',
                                 defaultColor: '#f5f5f5',
                               }}
-                                // Note: you will need to get a mapsApiKey for your project.
-                                // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
                                 mapsApiKey="YOUR_KEY_HERE"
                                 rootProps={{ 'data-testid': '1' }}
                             />
                         </div>
                         <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                            
+                        </div>
+                        
+                    </div>
+                    
+                    <div className="row">
+                        
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             
                         </div>
                         
