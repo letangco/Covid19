f2f445cc542ef5214df271fd2bb8826e6c7dce28
letangco <letangco@gmail.com>
2020-06-15 17:30:29 +0700

    Load PieCharStats nhưng chưa truyền dữ liệu vào

--- src/api/dashboard/index.js
+++ src/api/dashboard/index.js
@@ -7,9 +7,22 @@ export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
 
-    return data.map(({ confirmed, deaths, recovered,reportDate: date }) => 
+    return data.map((
+      { confirmed, deaths, recovered,reportDate: date }
+      ) => 
     ({ confirmed: confirmed.total, deaths: deaths.total,recovered: recovered.total, date }));
   } catch (error) {
     return error;
   }
 };
+
+//API lấy số liệu tổng quan của thế giới làm PieChart
+export const fetchStatsData =async ()=>{
+  try{
+    const{data} = await axios.get(`https://api.thevirustracker.com/free-api?global=stats`);
+    return data.results[0];
+  }
+  catch(error){
+    return error;
+  }
+}
--- src/components/dashboard/ChartDashBoard/PieChartStats.tsx
+++ src/components/dashboard/ChartDashBoard/PieChartStats.tsx
@@ -1,145 +1,98 @@
 import React, {Component} from 'react';
-import {fetchDailyData} from '../../../api/dashboard/index';
+import {fetchStatsData} from '../../../api/dashboard/index';
 import Chart from "react-apexcharts";
 interface Iprops{
 
 }
 interface Istate{
-    Date: any ,
-    Confirmed:any,
-    Death:any,
-    Recovered:any,
-    setDailyData:any ,
-    optionsMixedChart:any,
-    // seriesMixedChart:any
+    data:any ,
+    optionsMixedChart:any 
 }
 class PieChartStats extends Component <{}, Istate>{
     constructor(props :any){
         super(props);
         this.state={
-            Date:  [],
-            Confirmed:[],
-            Death:[],
-            Recovered: [],
-            setDailyData:[],
+            data: {},
             optionsMixedChart: {
+                series: [44, 55, 13, 43, 22],
                 chart: {
-                  id: "basic-bar",
-                  type: 'pie',
-                  toolbar: {
+                    width: 380,
+                    type: 'pie',
+                    toolbar: {
                     show: false
                   }
                 },
-                dataLabels: {
-                    enabled: false
-                  },
-                plotOptions: {
-                  bar: {
-                    columnWidth: "100%",
-                    endingShape: "arrow"
-                  }
-                },
-                stroke: {
-                    // Độ nét đứt - thưa của line )dashArray
-                    width: [5, 5, 5],
-                    curve: 'straight',
-                    dashArray: [1, 1, 1]
-                },
-                title: {
-                    text: 'Chart Covid 19 Stats',
-                    align: 'center'
-                },
-                xaxis: {
-                  categories: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-                },
-                markers: {
-                  size: 0,
-                  strokeWidth: 2,
-                  fillOpacity: 0,
-                  strokeOpacity: 0,
-                  hover: {
-                    size: 3,
-                  }
-                },
-                yaxis: {
-                  tickAmount: 10,
-                  min: 0,
-                  max: 8500000
-                }
+                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                      chart: {
+                        width: 200
+                      },
+                      legend: {
+                        position: 'bottom'
+                      }
+                    }
+                  }]
               },
-            //   seriesMixedChart: [
-            //     {
-            //         name: "Total Confirmed",
-            //         data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
-            //       },
-            //       {
-            //         name: "Page Views",
-            //         data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
-            //       },
-            //       {
-            //         name: 'Total Visits',
-            //         data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
-            //       }
-            //   ]
+            
         }
     }
     componentDidMount(){
         const fetchMyAPI = async () => {
-            const initialDailyData:any = await fetchDailyData();
-            this.setState({
-                setDailyData: initialDailyData
-            })
-            // Set Date
-            var arr: any = this.state.setDailyData;
-            var resultdate = arr.map((item :any)=>{
-                return item.date;
-            });
-            var resultConfirmed = arr.map((item :any)=>{
-                return item.confirmed;
-            });
-            var resultDeath = arr.map((item :any)=>{
-                return item.deaths;
-            });
-            var resultRecovered = arr.map((item :any)=>{
-                return item.recovered;
-            });
-            this.setState({
-                optionsMixedChart:{
-                    xaxis:{
-                        categories: resultdate
-                    }
-                },
-                Confirmed: resultConfirmed,
-                Death: resultDeath,
-                Recovered: resultRecovered
-            })
+            const initialDailyData:any = await fetchStatsData();
+            
+            console.log(initialDailyData)
+            
           };
           fetchMyAPI();
           
     }
     render (){
-        var seriesMixedChart =  [
-            {
-                name: "Total Confirmed",
-                data: this.state.Confirmed
-              },
-              {
-                name: "Total Deaths",
-                data: this.state.Death
-              },
-              {
-                name: 'Total Recovered',
-                data: this.state.Recovered
+        
+        var options = {
+            series: [44, 55, 13, 43, 22],
+            chart: {
+            width: 380,
+            toolbar: {
+                show: true,
+                offsetX: 0,
+                offsetY: 0,
+                tools: {
+                  download: true,
+                  selection: true,
+                  zoom: true,
+                  zoomin: true,
+                  zoomout: true,
+                  pan: true,
+                }
               }
-          ]
+        },
+          labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+          responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                    toolbar: {
+                    show: true
+                  }
+                },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }]
+          };
         return (
             <div>
                 <Chart
-                    options={this.state.optionsMixedChart}
-                    series={seriesMixedChart}
-                    type="line"
+                    options={options}
+                    series={options.series}
+                    type="donut"
                     width="500"
-                    height="400"
+                    height="500"
                 />
             </div>
         );
